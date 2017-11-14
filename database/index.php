bf76cfd316097a7d3b519e3acfcf9eef8d726168
AussieEevee <AussieEevee@outlook.com>
2017-11-15 05:59:32 +0800

    Update index.php

--- database/index.php
+++ database/index.php
@@ -5,9 +5,12 @@ require("etc/config.php") or die("Unable to load configuration file."); // Load 
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo $sitename; ?></title>
   <link rel="stylesheet" type="text/css" href="<?php echo $sitestylefile; ?>">
-  </head>
-  <body>
-  </body>
+</head>
+<body>
+    
+</body>
 </html>
