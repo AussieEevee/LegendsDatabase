8a93559d8c28016812a8adb20572f611351095a8
AussieEevee <AussieEevee@outlook.com>
2017-11-15 06:00:10 +0800

    Update index.php

--- database/index.php
+++ database/index.php
@@ -12,5 +12,21 @@ require("etc/config.php") or die("Unable to load configuration file."); // Load 
 </head>
 <body>
     
+<div class="header">
+  <h2>Header</h2>
+  <h3>CSS Template using Flexbox</h3>
+  <p>In this example, we have created a header, three unequal columns and a footer. On smaller screens, the columns will stack on top of each other. Resize the browser window to see the responsive effect.</p>
+  <p><strong>Note:</strong> Flexbox is not supported in Internet Explorer 10 eand earlier versions.</p>
+</div>
+
+<div class="row">
+  <div class="column side" style="background-color:#aaa;">Column</div>
+  <div class="column middle" style="background-color:#bbb;">Column</div>
+  <div class="column side" style="background-color:#ccc;">Column</div>
+</div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
 </body>
 </html>
