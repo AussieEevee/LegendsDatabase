037394edb988065f74fb4bcdc2b93763bfe3c2ee
AussieEevee <AussieEevee@outlook.com>
2017-11-15 05:22:30 +0800

    Create basic shell.

--- database/index.php
+++ database/index.php
@@ -2,3 +2,11 @@
 require("etc/config.php") or die("Unable to load configuration file."); // Load the configuration file.
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Test</title>
+  </head>
+  <body>
+  </body>
+</html>
